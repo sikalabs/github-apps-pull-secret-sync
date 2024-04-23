490e748d33576481a814f33c4f59ae306b04cbbe
Ondrej Sika <ondrej@ondrejsika.com>
2024-04-23 17:10:01 +0200

    feat: Run as service, update secrets every 50 minutes

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"os"
+	"time"
 
 	"github.com/sikalabs/github-apps-pull-secret-sync/pkg/ghcr"
 	"github.com/sikalabs/github-apps-pull-secret-sync/pkg/kubernetes"
@@ -14,9 +15,13 @@ func main() {
 		log.Fatalf("Usage github-apps-pull-secret-sync "+version.Version+": %s <githubAppID> <githubInstallationID> <privateKeyPath> <username> <namespace> [<namespace> ...]", os.Args[0])
 	}
 
-	token := ghcr.GetGhcrToken(os.Args[1], os.Args[2], os.Args[3])
-	dockerConfigJson := ghcr.CreateDockerConfigJson(os.Args[4], token)
-	for _, namespace := range os.Args[5:] {
-		kubernetes.CreateOrUpdareSecretDockerConfigJson("github-apps-pull-secret", namespace, dockerConfigJson)
+	for {
+		token := ghcr.GetGhcrToken(os.Args[1], os.Args[2], os.Args[3])
+		dockerConfigJson := ghcr.CreateDockerConfigJson(os.Args[4], token)
+		for _, namespace := range os.Args[5:] {
+			kubernetes.CreateOrUpdareSecretDockerConfigJson("github-apps-pull-secret", namespace, dockerConfigJson)
+		}
+		log.Println("Sleeping 50 minutes ...")
+		time.Sleep(50 * time.Minute)
 	}
 }
